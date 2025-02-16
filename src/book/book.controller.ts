ac4a517964054eeb0f6718dd9b7d79164f85dbb7
carlosriverogv <karlos.riverog@gmail.com>
2025-02-16 20:11:41 +0100

    Error control and documentation of services with JSDoc - book.service.ts

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -30,7 +30,7 @@ export class BookController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.bookService.findOne(id);
+    return this.bookService.findById(id);
   }
 
   @Patch(':id')
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -17,8 +17,16 @@ export class BookService {
     private readonly bookModel: Model<Book>,
   ) {}
 
-  // Servicio de inserción de nuevos libros
-  async create(createBookDto: CreateBookDto) {
+  /**
+   * @description Servicio de inserción de nuevos libros
+   * @param createBookDto - Datos del libro a insertar
+   * @returns {Promise<Book>} - Libro insertado
+   * @throws ConflictException - Si el libro ya existe
+   * @throws InternalServerErrorException - Si ocurre un error inesperado
+   */
+  async create(
+    createBookDto: CreateBookDto,
+  ): Promise<{ ok: boolean; resultado: Book }> {
     try {
       // Verificar si el libro ya existe
       const existingBook = await this.bookModel.findOne({
@@ -46,8 +54,12 @@ export class BookService {
     }
   }
 
-  // Servicio de búsqueda de todos los libros
-  async findAll() {
+  /**
+   * @description Servicio de búsqueda de todos los libros
+   * @returns {Promise<Book[]>} - Lista de libros encontrados
+   * @throws InternalServerErrorException
+   */
+  async findAll(): Promise<Book[]> {
     try {
       const resultado = await this.bookModel.find();
       return resultado || [];
@@ -59,60 +71,163 @@ export class BookService {
     }
   }
 
-  async findOne(id: string) {
-    const resultado = await this.bookModel.findById(id);
-    if (!resultado) {
-      throw new NotFoundException(`ID '${id}' de libro no encontrado`);
+  /**
+   * @description Servicio de búsqueda de libros por ID
+   * @param id - ID del libro a buscar
+   * @returns {Promise<Book>} - Libro encontrado
+   * @throws NotFoundException - Si el libro no existe
+   * @throws InternalServerErrorException - Si ocurre un error inesperado
+   */
+  async findById(id: string): Promise<Book> {
+    try {
+      const resultado = await this.bookModel.findById(id);
+      if (!resultado) {
+        throw new NotFoundException(`ID '${id}' de libro no encontrado`);
+      }
+      return resultado;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(error.message);
+      } else {
+        throw new InternalServerErrorException(
+          `Error inesperado buscando libro con ID '${id}'`,
+        );
+      }
     }
-    return resultado;
   }
 
-  async findByIsbn(isbn: string) {
-    const resultado = await this.bookModel.findOne({ isbn });
-    if (!resultado) {
-      throw new NotFoundException(`ISBN '${isbn}' de libro no encontrado`);
+  /**
+   * @description Servicio de búsqueda de libros por ISBN
+   * @param isbn - ISBN del libro a buscar
+   * @returns {Promise<Book>} - Libro encontrado
+   * @throws NotFoundException - Si el libro no existe
+   * @throws InternalServerErrorException - Si ocurre un error inesperado
+   */
+  async findByIsbn(isbn: string): Promise<Book> {
+    try {
+      const resultado = await this.bookModel.findOne({ isbn });
+      if (!resultado) {
+        throw new NotFoundException(`ISBN '${isbn}' de libro no encontrado`);
+      }
+      return resultado;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(error.message);
+      } else {
+        throw new InternalServerErrorException(
+          `Error inesperado buscando libro con ISBN '${isbn}'`,
+        );
+      }
     }
-    return resultado;
   }
 
-  async findByTitle(title: string) {
-    const resultado = await this.bookModel.find({ title });
-    if (!resultado) {
-      throw new NotFoundException(`Título '${title}' de libro no encontrado`);
+  /**
+   * @description Servicio de búsqueda de libros por título
+   * @param title - Título del libro a buscar
+   * @returns {Promise<Book[]>} - Lista de libros encontrados
+   * @throws InternalServerErrorException - Si ocurre un error inesperado
+   */
+  async findByTitle(title: string): Promise<Book[]> {
+    try {
+      const resultado = await this.bookModel.find({ title });
+      return resultado || [];
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `Error inesperado buscando libros con Título '${title}'`,
+      );
     }
-    return resultado || [];
   }
 
-  async findByAuthor(author: string) {
-    const resultado = await this.bookModel.find({ author });
-    if (!resultado) {
-      throw new NotFoundException(`Autor '${author}' de libro no encontrado`);
+  /**
+   * @description Servicio de búsqueda de libros por autor
+   * @param author - Autor del libro a buscar
+   * @returns {Promise<Book[]>} - Lista de libros encontrados
+   * @throws NotFoundException - Si el autor no existe
+   * @throws InternalServerErrorException - Si ocurre un error inesperado
+   */
+  async findByAuthor(author: string): Promise<Book[]> {
+    try {
+      const resultado = await this.bookModel.find({ author });
+      return resultado || [];
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `Error inesperado buscando libros con Autor '${author}'`,
+      );
     }
-    return resultado || [];
   }
 
-  async findByGender(gender: string) {
-    const resultado = await this.bookModel.find({ gender });
-    if (!resultado) {
-      throw new NotFoundException(`Género '${gender}' de libro no encontrado`);
+  /**
+   * @description Servicio de búsqueda de libros por género
+   * @param gender - Género del libro a buscar
+   * @returns {Promise<Book[]>} - Lista de libros encontrados
+   * @throws InternalServerErrorException - Si ocurre un error inesperado
+   */
+  async findByGender(gender: string): Promise<Book[]> {
+    try {
+      const resultado = await this.bookModel.find({ gender });
+      return resultado || [];
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `Error inesperado buscando libros con Género '${gender}'`,
+      );
     }
-    return resultado || [];
   }
 
-  async update(id: string, updateBookDto: UpdateBookDto) {
-    const bookUpdated = await this.bookModel.findByIdAndUpdate(
-      id,
-      { $set: updateBookDto },
-      { new: true },
-    );
-    return bookUpdated;
+  /**
+   * @description Servicio de actualización de libros
+   * @param id - ID del libro a actualizar
+   * @param updateBookDto - Datos del libro a actualizar
+   * @returns {Promise<Book>} - Libro actualizado
+   * @throws NotFoundException - Si el libro no existe
+   * @throws InternalServerErrorException - Si ocurre un error inesperado
+   */
+  async update(id: string, updateBookDto: UpdateBookDto): Promise<Book> {
+    try {
+      const bookUpdated = await this.bookModel.findByIdAndUpdate(
+        id,
+        { $set: updateBookDto },
+        { new: true },
+      );
+      if (!bookUpdated) {
+        throw new NotFoundException(`ID '${id}' de libro no encontrado`);
+      }
+      return bookUpdated;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(error.message);
+      } else {
+        throw new InternalServerErrorException(
+          `Error inesperado actualizando el libro con ID '${id}'`,
+        );
+      }
+    }
   }
 
-  async remove(id: string) {
-    const resultado = await this.bookModel.findByIdAndDelete(id);
-    if (!resultado) {
-      throw new NotFoundException(`ID '${id}' de libro no encontrado`);
+  /**
+   * @description Servicio de eliminación de libros
+   * @param id - ID del libro a eliminar
+   * @returns {Promise<Book>} - Libro eliminado
+   * @throws NotFoundException - Si el libro no existe
+   * @throws InternalServerErrorException - Si ocurre un error inesperado
+   */
+  async remove(id: string): Promise<Book> {
+    try {
+      const resultado = await this.bookModel.findByIdAndDelete(id);
+      if (!resultado) {
+        throw new NotFoundException(`ID '${id}' de libro no encontrado`);
+      }
+      return resultado;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(error.message);
+      } else {
+        throw new InternalServerErrorException(
+          `Error inesperado borrando el libro con ID '${id}'`,
+        );
+      }
     }
-    return resultado;
   }
 }
