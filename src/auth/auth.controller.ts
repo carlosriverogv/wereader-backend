fcf4e3394553bf2640e384e6310032a924b8bcd5
carlosriverogv <karlos.riverog@gmail.com>
2025-06-11 16:45:57 +0200

    feat: Añadir autenticación en el registro de usuario (TEMPORAL por seguridad)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { UserService } from 'src/user/user.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from './guards/auth.guard';
 
 @ApiTags('Autenticación')
+@ApiBearerAuth()
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -39,6 +41,7 @@ export class AuthController {
    * @description Registro de usuario
    * @returns El usuario
    */
+  @UseGuards(AuthGuard)
   @Post('register')
   @ApiOperation({ summary: 'Registrar un usuario (con biblioteca)' })
   async register(@Body() createUserDto: CreateUserDto) {
